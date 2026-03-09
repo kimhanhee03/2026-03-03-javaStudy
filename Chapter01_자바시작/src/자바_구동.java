e359aec7b4e836fbed6193883df7f5d634027da8
sist-9 <sist-9@sist9>
2026-03-09 17:46:16 +0900

    0000

---
+++ Chapter01_자바시작/src/자바_구동.java
@@ -0,0 +1,16 @@
+/*
+ *  규칙(개발자의 약속
+ *   1. 클래스명: 첫자 대문자 ex: filename => FileName, 한글사용 가능(영문 권장)
+ *   2. 변수명 : 소문자 시작 
+ *   3. 상수명 : 모든 알파벳을 대문자
+ *   4. 메소드 : 소문자 시작
+ */
+public class 자바_구동 {
+
+	public static void main(String[] args) {
+		System.out.println("자바 실행"); // ()안에 있는 데이터를 출력
+		System.out.println("<html><body><h1>Hello Java");
+		
+	}
+	
+}
---
+++ Chapter01_자바시작/src/자바_시작.java
@@ -0,0 +1,23 @@
+/*
+ *  자바 => 모든 소스는 클래스안에 첨부
+ *      => JVM이 가장 먼저 실행하는 부분 => main (진입점)
+ *  이미 만들어진 클래스 읽기 (개발자,자바 제공)
+ *    ㄴ import
+ *      오버라이딩 : 미국식을 우리나라식으로 변경
+ *      public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 
+		public static void main(String[] a) {
+		// TODO Auto-generated method stub
+		
+		static public void main(String[] args) {
+		// TODO Auto-generated method stub
+ */
+public class 자바_시작 {
+
+	static public void main(String[] a) {
+		// TODO Auto-generated method stub
+           System.out.println("Hello Java");
+	}
+
+}
